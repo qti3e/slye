6b787d23298836a7cf116b117652950d3de4c91f
Parsa Ghadimi <me@qti3e.com>
2019-05-08 19:48:18 +0430

    Actions: Transform and delete component

--- core/actions.ts
+++ core/actions.ts
@@ -9,6 +9,7 @@
  */
 
 import { Step } from "./step";
+import { Component } from "./component";
 import { Presentation } from "./presentation";
 
 interface Action<P, T> {
@@ -16,11 +17,39 @@ interface Action<P, T> {
   readonly backward: (data: T) => void;
 }
 
+type TransformForwardData = {
+  object: Step | Component;
+  prevX: number;
+  prevY: number;
+  prevZ: number;
+  x: number;
+  y: number;
+  z: number;
+};
+
+type TransformBackwardData = {
+  object: Step | Component;
+  prevX: number;
+  prevY: number;
+  prevZ: number;
+};
+
 export interface ActionTypes {
-  readonly DELETE_STEP: Action<
+  DELETE_STEP: Action<
     { step: Step },
     { step: Step; presentation: Presentation; index: number }
   >;
+  DELETE_COMPONENT: Action<
+    { component: Component },
+    { component: Component; step: Step }
+  >;
+  UPDATE_POSITION: Action<TransformForwardData, TransformBackwardData>;
+  UPDATE_ROTATION: Action<TransformForwardData, TransformBackwardData>;
+  UPDATE_SCALE: Action<TransformForwardData, TransformBackwardData>;
+  // TODO(qti3e)
+  // NEW_STEP: Creates a new step
+  // INSERT_COMPONENT: Add a new component to the step.
+  // UPDATE_PROPS: Update component props.
 }
 
 export type ForwardData<
@@ -49,6 +78,43 @@ export const actions: ActionsMap = {
     },
     backward({ index, presentation, step }) {
       presentation.add(step, index);
+    }
+  },
+  DELETE_COMPONENT: {
+    forward({ component }) {
+      const step = component.owner;
+      step.del(component);
+      return { step, component };
+    },
+    backward({ component, step }) {
+      step.add(component);
+    }
+  },
+  UPDATE_POSITION: {
+    forward({ object, prevX, prevY, prevZ, x, y, z }) {
+      object.setPosition(x, y, z);
+      return { object, prevX, prevY, prevZ };
+    },
+    backward({ object, prevX, prevY, prevZ }) {
+      object.setPosition(prevX, prevY, prevZ);
+    }
+  },
+  UPDATE_ROTATION: {
+    forward({ object, prevX, prevY, prevZ, x, y, z }) {
+      object.setRotation(x, y, z);
+      return { object, prevX, prevY, prevZ };
+    },
+    backward({ object, prevX, prevY, prevZ }) {
+      object.setRotation(prevX, prevY, prevZ);
+    }
+  },
+  UPDATE_SCALE: {
+    forward({ object, prevX, prevY, prevZ, x, y, z }) {
+      object.setScale(x, y, z);
+      return { object, prevX, prevY, prevZ };
+    },
+    backward({ object, prevX, prevY, prevZ }) {
+      object.setScale(prevX, prevY, prevZ);
     }
   }
 };
